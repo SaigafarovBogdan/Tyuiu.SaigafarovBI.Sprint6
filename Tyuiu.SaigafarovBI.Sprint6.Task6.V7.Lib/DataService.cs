9b94b4bcfc9d7a9038cae74c328bc839d7bd955c
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-10-02 13:04:54 +0500

    Task6

--- Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib/DataService.cs
@@ -8,20 +8,19 @@ namespace Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib
 
         public string CollectTextFromFile(string path)
         {
-            //string result = "";
-            //using (StreamReader reader = new StreamReader(path))
-            //{
-            //    string? line;
-            //    string[] fragments;
-            //    while ((line = reader.ReadLine()) != null)
-            //    {
-            //        fragments = line.Split(' ');
-            //        result += fragments.Length > 2 ? fragments[2] + " " : "";
-            //    }
-            //}
-            //result = result.Trim();
-            //return result;
-            return "cHshl fGf bBYQQ pmYGtO xakZKciG";
+            string result = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                string[] fragments;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fragments = line.Split(' ');
+                    result += fragments.Length > 2 ? fragments[2] + " " : "";
+                }
+            }
+            result = result.Trim();
+            return result;
         }
     }
 }
