c804aee4b0f8980ec25575c6f846679bab2128ea
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-10-02 09:24:07 +0500

    Task6

--- Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib/DataService.cs
+++ Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib/DataService.cs
@@ -5,7 +5,8 @@ namespace Tyuiu.SaigafarovBI.Sprint6.Task6.V7.Lib
 {
     public class DataService : ISprint6Task6V7
     {
-        public string CollectTextFromFile(string str, string path)
+
+        public string CollectTextFromFile(string path)
         {
             string result = "";
             using (StreamReader reader = new StreamReader(path))
