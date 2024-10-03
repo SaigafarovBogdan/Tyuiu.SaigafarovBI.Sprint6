d10bd760242c8157815b2e536c98d06976ff030f
Tyuiu.SaigafarovBI@outlook.com <sajgafarovb@gmail.com>
2024-10-03 10:30:19 +0500

    Task7

--- Tyuiu.SaigafarovBI.Sprint6.Task7.V21.Test/DataServiceTest.cs
+++ Tyuiu.SaigafarovBI.Sprint6.Task7.V21.Test/DataServiceTest.cs
@@ -1,11 +1,27 @@
+using Tyuiu.SaigafarovBI.Sprint6.Task7.V21.Lib;
+
 namespace Tyuiu.SaigafarovBI.Sprint6.Task7.V21.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidFile()
         {
+            Assert.AreEqual(true,File.Exists(@"C:\Users\User\source\repos\Tyuiu.SaigafarovBI.Sprint6\gg.csv"));
+        }
+
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService dataService = new DataService();
+            string path = @"C:\Users\User\source\repos\Tyuiu.SaigafarovBI.Sprint6\gg.csv";
+            int[,] wait = new int[2, 9]
+            {
+                { 9,3,1,25,125,12,5,-1,5},
+                { 124,2,12,25,6,1,1,-1,1}
+            };
+            CollectionAssert.AreEqual(dataService.GetMatrix(path), wait);
         }
     }
 }
